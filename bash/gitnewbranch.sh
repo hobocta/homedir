073f975d61f03efb91a91a01c975b77d3cc79cf4
Антон Кондратов <contact@site-home.ru>
2016-04-19 18:45:49 +0300

    gitnewbranch.sh

--- bash/gitnewbranch.sh
+++ bash/gitnewbranch.sh
@@ -2,4 +2,11 @@
 
 branch="$1"
 
+if [ -z "$branch" ]; then
+    echo "Not specified branch"
+    exit
+fi
+
+branch=$(echo $branch | sed 's/-/_/g')
+
 git co dev && git pull && git co -b $branch && git push origin $branch && git br --set-upstream-to=origin/$branch
