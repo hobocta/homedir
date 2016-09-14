a66abea5a5eb3645d31170aa4086c4da6e50fa29
Антон Кондратов <contact@site-home.ru>
2016-09-14 12:39:01 +0300

    gitmultiprune

--- bash/gitmultiprune.sh
+++ bash/gitmultiprune.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 
-find -mindepth 2 -maxdepth 2 -type d -name '.git' -exec sh -c 'cd {} && cd .. && pwd && git remote prune origin' \;
+find -mindepth 2 -maxdepth 2 -type d -name '.git' -exec sh -c 'cd {} && cd .. && pwd && git remote prune origin && git branch -vv | grep ": gone]" | awk "{print $1}" | xargs --no-run-if-empty git branch -d' \;
