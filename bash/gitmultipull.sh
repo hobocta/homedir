28dbb0d9154fa01b713b8b36a29ae3312be9218b
Антон Кондратов <contact@site-home.ru>
2016-09-15 15:11:36 +0300

    gitmultipull

--- bash/gitmultipull.sh
+++ bash/gitmultipull.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-find -mindepth 2 -maxdepth 2 -type f -name '.hobocta.git.pull.on' -mmin +120 -exec sh -c '\
+find -mindepth 2 -maxdepth 2 -type f -name '.hobocta.git.pull.on' -mmin +720 -exec sh -c '\
 		cd $(dirname "{}") \
 		&& echo -n "${PWD##*/}: " \
 		&& git pull -n \
