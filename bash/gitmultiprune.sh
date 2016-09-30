bcae0101bb38862c44d6e3f2066df08e35ecec0d
Антон Кондратов <contact@site-home.ru>
2016-09-30 12:42:15 +0300

    bash

--- bash/gitmultiprune.sh
+++ bash/gitmultiprune.sh
@@ -2,7 +2,9 @@
 
 find -mindepth 2 -maxdepth 2 -type f -name '.hobocta.git.prune.on' -mmin +720 -exec sh -c '\
 		cd $(dirname "{}") \
+		&& echo -n "$(tput setaf 4)" \
 		&& echo "${PWD##*/}" \
+		&& echo -n "$(tput sgr0)" \
 		&& git remote prune origin \
 		&& git branch -vv | grep -v "*" | grep ": gone]" | awk "{print \$1}" | xargs --no-run-if-empty git branch -d \
 		&& touch $(basename "{}") \
--- bash/gitmultipull.sh
+++ bash/gitmultipull.sh
@@ -2,7 +2,9 @@
 
 find -mindepth 2 -maxdepth 2 -type f -name '.hobocta.git.pull.on' -mmin +720 -exec sh -c '\
 		cd $(dirname "{}") \
-		&& echo -n "${PWD##*/}: " \
+		&& echo -n "$(tput setaf 4)" \
+		&& echo "${PWD##*/}" \
+		&& echo -n "$(tput sgr0)" \
 		&& git pull -n \
 		&& touch $(basename "{}") \
 	' \;
