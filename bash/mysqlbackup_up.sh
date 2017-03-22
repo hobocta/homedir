c5bec28447b193b3a968b7db1808eb690e244bbc
Антон Кондратов <contact@site-home.ru>
2017-03-23 02:27:05 +0300

    mysqlbackup_up

--- bash/mysqlbackup_up.sh
+++ bash/mysqlbackup_up.sh
@@ -8,6 +8,53 @@ for file in `find . -name '*.sql.gz'`; do
 	files+=($file)
 done
 
+dbNames=()
+
+in_array() {
+	local haystack=${1}[@]
+	local needle=${2}
+	for i in ${!haystack}; do
+		if [[ ${i} == ${needle} ]]; then
+			return 0
+		fi
+	done
+	return 1
+}
+
+counter=0
+
+for file in "${files[@]}"
+do
+	let counter=counter+1
+
+	IFS='.' read directory dbName tableName ext gz <<< "$file"
+	IFS='/' read directory dbName <<< "$dbName"
+
+	in_array dbNames "${dbName}" && echo -n '' || dbNames+=($dbName)
+
+	let zero=$counter%100
+
+	if [ $zero = 0 ]; then
+		echo -n '.'
+	fi
+done
+
+echo ''
+
+dbNamesCount=${#dbNames[@]}
+dbNamesCounter=0
+
+for dbName in "${dbNames[@]}"
+do
+	let dbNamesCounter=dbNamesCounter+1
+
+	createDbComand='CREATE DATABASE IF NOT EXISTS '$dbName';'
+
+	printf '[%02d/%02d] %-50s...' "$dbNamesCounter" "$dbNamesCount" "$createDbComand"
+	echo $createDbComand | mysql -u root
+	echo 'done'
+done
+
 filesCount=${#files[@]}
 filesCounter=0
 
