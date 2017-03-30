7dfd48bcac50b284baba91a28123d62a0b5b9894
Антон Кондратов <contact@site-home.ru>
2017-03-30 22:40:30 +0300

    projectsbackup_up

---
+++ bash/projectsbackup_up.sh
@@ -0,0 +1,23 @@
+#!/bin/bash
+
+cd /e/projects-backup
+
+archives=()
+
+for archive in `find -maxdepth 1 -name '*.tar.gz'`; do
+	archives+=($archive)
+done
+
+count=${#archives[@]}
+counter=0
+
+for archive in "${archives[@]}"
+do
+	let counter=counter+1
+
+	printf '[%02d/%02d] %-50s...' "$counter" "$count" "$archive"
+
+	tar xzf $archive -C /
+
+	echo 'done'
+done
