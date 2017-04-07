3faa375dafc93e5bb203b5f13a3fa38f02463cb4
Антон Кондратов <contact@site-home.ru>
2017-04-07 21:15:04 +0300

    projectsbackup

--- bash/projectsbackup.sh
+++ bash/projectsbackup.sh
@@ -10,6 +10,7 @@ for link in `find -L /f/OpenServer/domains -mindepth 1 -maxdepth 1 -xtype l`; do
 	IFS=' ' read from separator to <<< "$two"
 	IFS='/' read fromDisk fromOpenServer fromDomains from <<< "$from"
 	IFS='/' read empty toDisk toPath <<< "$to"
+	echo 'del /F /Q /S '$from >> $domainsLinksFile
 	echo 'mklink /D '$from' "'$toDisk':\'${toPath//\//\\}'"' >> $domainsLinksFile
 done
 
@@ -21,6 +22,7 @@ for link in `find -L /f/projects -mindepth 1 -maxdepth 3 -xtype l`; do
 	IFS=' ' read from separator to <<< "$two"
 	IFS='/' read fromDisk fromPath <<< "$from"
 	IFS='/' read empty toDisk toPath <<< "$to"
+	echo 'del /F /Q /S '$fromDisk':\'${fromPath//\//\\} >> $domainsLinksFile
 	echo 'mklink /D '$fromDisk':\'${fromPath//\//\\}' "'$toDisk':\'${toPath//\//\\}'"' >> $projectsLinksFile
 done
 
